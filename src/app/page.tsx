8aeb4668be0483af8ac670263b83a8304f923a14
shubh solanki <145123927+shubhisme@users.noreply.github.com>
2025-03-28 06:28:43 +0530

    happy birthday commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import HomePageComponent from "@/app/Home/page";
 export default function HomePage() {
   return (
     <>
-      {/* <InternationalAdmissions></InternationalAdmissions> */}
       <HomePageComponent />
       <SignInButton mode="redirect" forceRedirectUrl="/Home">
         <button className="btn btn-primary">Sign In</button>
